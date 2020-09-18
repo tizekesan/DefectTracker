2845de48ed0009bfed3fe0bf21bcc7b182a75e90
unknown <you@example.com>
2020-09-18 18:44:30 +0530

    Project setup commit

---
+++ src/main/java/com/example/demo/DefectTrackerAlphaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefectTrackerAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefectTrackerAlphaApplication.class, args);
+	}
+
+}
